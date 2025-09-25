bbaab1bc3c2a7d1519f2dfaf552fb4461bde82c0
rnegi <rnegi@finvasia.in>
2025-09-25 16:05:22 +0530

    feat: removed all files and keeping static folder outside

--- src/constant/constants.ts
+++
@@ -1,3 +0,0 @@
-const SLOW_THRESHOLD = 2000; //2s
-
-export { SLOW_THRESHOLD };
--- utils/NumberFormatting.ts
+++
@@ -1,7 +0,0 @@
-export function formatNumberWithCommas(number: number | string) {
-  // Convert number to a comma seperated US standard and no decimals
-  const formattedNumber = Number(number).toLocaleString("en-US", {
-    maximumFractionDigits: 0,
-  });
-  return formattedNumber;
-}
